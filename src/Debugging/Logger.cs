d9f3b4e6f63039a065daab00919927543dd44603
Charlotte C. Brown <charlotte.clover.brown@brinx.ca>
2019-02-08 23:34:51 -0500

    Renamed because of gitignore not allowing these to be pushed.

---
+++ src/Debugging/Logger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace CCB
+{
+	namespace DataCore
+	{
+		namespace Debugging
+		{
+			public static class ConsoleUtils
+			{
+				public static void Message(string info, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "")
+				{
+					UnityEngine.Debug.Log(GetFormattedMessage(memberName, lineNumber, filePath, info, "Info"));
+				}
+
+				public static void Warning(string info, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "")
+				{
+					UnityEngine.Debug.LogWarning(GetFormattedMessage(memberName, lineNumber, filePath, info, "Warning"));
+				}
+
+				public static void Error(string info, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "")
+				{
+					UnityEngine.Debug.LogError(GetFormattedMessage(memberName, lineNumber, filePath, info, "Error"));
+				}
+
+				private static string GetFormattedMessage(string memberName, int lineNumber, string filePath, string info, string label)
+				{
+					return $"{Application.productName} ({Path.GetFileNameWithoutExtension(filePath)}::{memberName} - {lineNumber}) [{label}] {info}";
+				}
+			}
+		}
+	}
+}
---
+++ src/Debugging/Reflection/Utilities.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+namespace CCB
+{
+	namespace DataCore
+	{
+		namespace Debugging
+		{
+			namespace Reflection
+			{
+				public static class Utilities
+				{
+					/// <summary>
+					/// If possible, retrieves the field name from the owner that the reference
+					/// object is stored within.
+					/// </summary>
+					public static string GetFieldName(object referenceObject, object owner)
+					{
+						string fieldName = "";
+
+						FieldInfo[] fields = owner.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.IgnoreCase);
+
+						foreach (FieldInfo field in fields)
+						{
+							if (referenceObject.Equals(field.GetValue(owner)))
+							{
+								fieldName = field.Name;
+							}
+						}
+
+						return fieldName;
+					}
+				}
+			}
+		}
+	}
+}
